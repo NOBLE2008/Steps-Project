7bde9f78ef2c5543a2928980d4041a2add17ce60
Noble Benjamin <nobleben2008@gmail.com>
2024-06-09 17:31:43 +0100

    Added a div for numbers

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ function App() {
   return (
     <div className="container">
       <div className="steps">
-
+        <div className='numbers'>
+          <div className="number"><p>1</p></div>
+          <div className="number"><p>2</p></div>
+          <div className="number"><p>3</p></div>
+        </div>
       </div>
     </div>
   )
