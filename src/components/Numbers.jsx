be7e5d1bc2152927dd0089309ea462233480b7cb
Noble Benjamin <nobleben2008@gmail.com>
2024-06-10 23:49:44 +0100

    Removed console.log in Numbers Component

--- src/components/Numbers.jsx
+++ src/components/Numbers.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const Numbers = (props) => {
-  console.log(props.page, props.index+1)
   return (
     <div className={`number${props.page === props.index+1 ? ' active' : ''}`}><p>{props.index + 1}</p></div>
   )
