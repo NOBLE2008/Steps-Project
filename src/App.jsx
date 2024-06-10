31e3d8077e60ff5f2d9cf12e0e2f7fbd853949d8
Noble Benjamin <nobleben2008@gmail.com>
2024-06-10 23:19:32 +0100

    Added a steps array

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from 'react'
 import './index.css'
+import Numbers from './components/Numbers'
 
 function App() {
 
   const {page, setPage} = useState(1)
 
+  const steps = [
+    `Step 1: Start Computer`,
+    `Step 2: Open Ms Word`,
+    `Step 3: Click on Editor`,
+  ]
+
   return (
     <div className="container">
       <div className="steps">
         <div className='numbers'>
-          
         </div>
         <div className="step">
           <p>Step 1: Press the windows button</p>
